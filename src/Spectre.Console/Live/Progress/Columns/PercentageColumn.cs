3852b3e9d761db9c88f82d8aaec2aec5601a454f
Yuuki Wesp <ls-micro@ya.ru>
2022-12-30 17:11:30 +0300

    fixes

--- src/Spectre.Console/Live/Progress/Columns/PercentageColumn.cs
+++ src/Spectre.Console/Live/Progress/Columns/PercentageColumn.cs
@@ -15,22 +15,20 @@ public sealed class PercentageColumn : ProgressColumn
     /// </summary>
     public Style CompletedStyle { get; set; } = new Style(foreground: Color.Green);
 
-        /// <summary>
-        /// Gets or sets the style for a failed task.
-        /// </summary>
-        public Style FailedStyle { get; set; } = new Style(foreground: Color.Red);
+    /// <summary>
+    /// Gets or sets the style for a failed task.
+    /// </summary>
+    public Style FailedStyle { get; set; } = new Style(foreground: Color.Red);
 
-        /// <inheritdoc/>
-        public override IRenderable Render(RenderContext context, ProgressTask task, TimeSpan deltaTime)
-        {
-            var percentage = (int)task.Percentage;
-            if (task.IsFailed)
-            {
-                return new Text($"{percentage}%", FailedStyle).RightAligned();
-            }
-            var style = percentage == 100 ? CompletedStyle : Style ?? Style.Plain;
-            return new Text($"{percentage}%", style).RightAligned();
-        }
+    /// <inheritdoc/>
+    public override IRenderable Render(RenderOptions options, ProgressTask task, TimeSpan deltaTime)
+    {
+        var percentage = (int)task.Percentage;
+        if (task.IsFailed)
+            return new Text($"{percentage}%", FailedStyle).RightJustified();
+        var style = percentage == 100 ? CompletedStyle : Style ?? Style.Plain;
+        return new Text($"{percentage}%", style).RightJustified();
+    }
 
     /// <inheritdoc/>
     public override int? GetColumnWidth(RenderOptions options)
